3aeb3a04e3d50548d6bb6b29dec2f8740aa1ea75
JSF 223 Weird <justinlatta6@gmail.com>
2016-10-19 22:04:04 -0600

    Updated Version Numbers

--- src/main/java/com/weirdest/weirdforce/Main.java
+++ src/main/java/com/weirdest/weirdforce/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	//Do i really need to explain this?
     public static final String MODID = "weirdforce";
     public static final String MODNAME = "The Weird Force Mod";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.1";
     
     //Make it now so forge won't :)
     @Instance
