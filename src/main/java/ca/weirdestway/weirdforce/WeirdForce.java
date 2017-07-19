2f9627beeed0f1c18d4af516472a860256b40c76
Justin <justinlatta6@gmail.com>
2017-07-19 17:52:32 -0600

    Release!

--- src/main/java/ca/weirdestway/weirdforce/WeirdForce.java
+++ src/main/java/ca/weirdestway/weirdforce/WeirdForce.java
@@ -17,7 +17,7 @@ public class WeirdForce {
 	//Do i really need to explain this?
     public static final String MODID = "weirdforce";
     public static final String MODNAME = "The Weird Force Mod";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
     
     //Create config object
     public static Configuration config;
